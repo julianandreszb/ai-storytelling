e9bc4789b1429623eb95dedd97a53a58c6ee8682
Julian Zapata <julianandreszb@gmail.com>
2024-04-05 18:33:15 -0500

    feat: initialize vue project

--- src/App.vue
+++ src/App.vue
@@ -21,11 +21,6 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
   width: 100%;
   font-size: 12px;
@@ -56,10 +51,6 @@ nav a:first-of-type {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,47 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 defineProps<{
   msg: string
 }>()
 
-const CLOUDFLARE_WORKERS_AI_TOKEN = import.meta.env.VITE_CLOUDFLARE_WORKERS_AI_TOKEN
-const CLOUDFLARE_WORKERS_AI_CLIENT_ID = import.meta.env.VITE_CLOUDFLARE_WORKERS_CLIENT_ID
+// const CLOUDFLARE_WORKERS_AI_TOKEN = import.meta.env.VITE_CLOUDFLARE_WORKERS_AI_TOKEN
+// const CLOUDFLARE_WORKERS_AI_CLIENT_ID = import.meta.env.VITE_CLOUDFLARE_WORKERS_CLIENT_ID
+//
+// fetch(`https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_WORKERS_AI_CLIENT_ID}/ai/run/@cf/meta/llama-2-7b-chat-int8`, {
+//   method: "POST",
+//   headers: {
+//     "Authorization": `Bearer ${CLOUDFLARE_WORKERS_AI_TOKEN}`,
+//     "Content-Type": "application/json"
+//   },
+//   body: JSON.stringify({ prompt: "Where did the phrase Hello World come from" })
+// })
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch((error) => console.error('Error:', error));
 
-fetch(`https://api.cloudflare.com/client/v4/accounts/${CLOUDFLARE_WORKERS_AI_CLIENT_ID}/ai/run/@cf/meta/llama-2-7b-chat-int8`, {
-  method: "POST",
-  headers: {
-    "Authorization": `Bearer ${CLOUDFLARE_WORKERS_AI_TOKEN}`,
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({ prompt: "Where did the phrase Hello World come from" })
-})
+const workerResponse = ref('')
+
+// fetch GET
+/*fetch('https://hello-ai.julianandreszb.workers.dev/')
   .then(response => response.json())
-  .then(data => console.log(data))
-  .catch((error) => console.error('Error:', error));
+  .then(data => workerResponse.value = data.message)*/
+
+async function fetchWorker() {
+  const response = await fetch('https://hello-ai.julianandreszb.workers.dev/')
+  const data = await response.json()
+  console.log(data)
+  workerResponse.value = data.response
+}
+
+fetchWorker()
 
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+    <h2>{{ workerResponse }}</h2>
   </div>
 </template>
 
